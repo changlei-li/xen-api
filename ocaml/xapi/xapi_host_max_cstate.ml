7676555f84648643a2697e494199bd9298e6e506
Changlei Li <changlei.li@cloud.com>
2025-09-19 10:51:44 +0800

    CP-54473 Implement max-state setting
    Two commands are used to set max_cstate: xenpm to set at runtime
    and xen-cmdline to set it in grub conf file to take effect after
    reboot.
    Set sub cstate limit to 0 to restrict CPU on base C<N> state.
    
    Signed-off-by: Changlei Li <changlei.li@cloud.com>

---
+++ ocaml/xapi/xapi_host_max_cstate.ml
@@ -0,0 +1,114 @@
+(*
+ * Copyright (c) Cloud Software Group, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+module D = Debug.Make (struct let name = "xapi_host_max_cstate" end)
+
+open D
+
+(* xenpm examples:
+   # xenpm set-max-cstate 0 0
+   max C-state set to C0
+   max C-substate set to 0 succeeded
+   # xenpm set-max-cstate 0
+   max C-state set to C0
+   max C-substate set to unlimited succeeded
+   # xenpm set-max-cstate unlimited
+   max C-state set to unlimited
+   # xenpm set-max-cstate -1
+   Missing, excess, or invalid argument(s)
+*)
+let xenpm_set value =
+  let args =
+    match value with
+    | -1L ->
+        ["set-max-cstate"; "unlimited"]
+    | n when n >= 0L ->
+        ["set-max-cstate"; Int64.to_string value; "0"]
+    | _ ->
+        raise
+          Api_errors.(
+            Server_error (invalid_value, ["value"; Int64.to_string value])
+          )
+  in
+  let resp = Helpers.call_script !Xapi_globs.xenpm_bin args in
+  (* Check runtime set max_cstate result *)
+  let cstate =
+    try Scanf.sscanf resp "max C-state set to %s" Fun.id
+    with Scanf.Scan_failure _ ->
+      error "Failed to parse max_cstate response: %s" resp ;
+      raise Api_errors.(Server_error (invalid_value, ["value"; resp]))
+  in
+  let value_in_resp =
+    match cstate with
+    | "unlimited" ->
+        -1L
+    | s -> (
+      try Scanf.sscanf s "C%Ld" Fun.id
+      with Scanf.Scan_failure _ ->
+        error "Failed to parse max_cstate response: %s" resp ;
+        raise Api_errors.(Server_error (invalid_value, ["value"; resp]))
+    )
+  in
+  if value_in_resp <> value then (
+    error "Failed to set max_cstate: expected %Ld, got %Ld" value value_in_resp ;
+    raise
+      Api_errors.(
+        Server_error
+          ( invalid_value
+          , [
+              "value"
+            ; Int64.to_string value_in_resp
+            ; "expected"
+            ; Int64.to_string value
+            ]
+          )
+      )
+  )
+
+let xen_cmdline_set value =
+  let args =
+    match value with
+    | -1L ->
+        ["--delete-xen"; "max_cstate"]
+    | n when n >= 0L ->
+        ["--set-xen"; Printf.sprintf "max_cstate=%Ld,0" n]
+    | _ ->
+        raise
+          Api_errors.(
+            Server_error (invalid_value, ["value"; Int64.to_string value])
+          )
+  in
+  Helpers.call_script !Xapi_globs.xen_cmdline_script args |> ignore
+
+let xen_cmdline_get () =
+  let args = ["--get-xen"; "max_cstate"] in
+  try
+    let ret =
+      Helpers.call_script !Xapi_globs.xen_cmdline_script args |> String.trim
+    in
+    (* the ret may be
+       "" -> unlimited
+       "max_cstate=N" -> max cstate N
+       "max_cstate=N,M" -> max cstate N, max c-sub-state M *)
+    match Astring.String.fields ~is_sep:(fun c -> c = '=' || c = ',') ret with
+    | [""] ->
+        -1L
+    | ["max_cstate"; state] | ["max_cstate"; state; _] ->
+        Int64.of_string state
+    | _ ->
+        error "Failed to parse max_cstate response: %s" ret ;
+        Helpers.internal_error "Failed to get max_cstate"
+  with e ->
+    error "Failed to get max_cstate: %s" (Printexc.to_string e) ;
+    Helpers.internal_error "Failed to get max_cstate"
---
+++ ocaml/xapi/xapi_host_max_cstate.mli
@@ -0,0 +1,53 @@
+(*
+ * Copyright (c) Cloud Software Group, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+(** Host CPU C-state management functions.
+
+    C-states are power management states for CPUs where higher numbered states
+    represent deeper sleep modes with lower power consumption but higher wake-up
+    latency. The max_cstate parameter controls the deepest C-state that CPUs
+    are allowed to enter.
+
+    Common C-state values:
+    - C0: CPU is active (not a sleep state)
+    - C1: CPU is halted but can wake up almost instantly
+    - C2: CPU caches are flushed, slightly longer wake-up time
+    - C3+: Deeper sleep states with progressively longer wake-up times
+
+    Setting max_cstate=1 restricts CPUs to only use C0 and C1 states,
+    which can improve performance for latency-sensitive workloads at the
+    cost of higher power consumption.
+*)
+
+(* Please note that the sub cstate limit to 0 to restrict CPU on base C<N> state *)
+
+val xenpm_set : int64 -> unit
+(** [xenpm_set value] sets the maximum C-state using the xenpm tool.
+    This affects the runtime power management behavior immediately.
+    
+    @param value The maximum C-state value
+    @raise Failure if the xenpm command fails *)
+
+val xen_cmdline_set : int64 -> unit
+(** [xen_cmdline_set value] sets the max_cstate parameter in the Xen command line.
+    This setting will take effect on the next reboot.
+    
+    @param value The maximum C-state value
+    @raise Failure if the command line fails *)
+
+val xen_cmdline_get : unit -> int64
+(** [xen_cmdline_get ()] retrieves the current max_cstate setting from the Xen command line.
+    
+    @return The current max_cstate value, or -1L if not configured
+    @raise Failure if reading the command line fails or parsing fails *)
